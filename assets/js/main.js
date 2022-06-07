92fcca67e5440604a7f8d4fbe14538714dbed61b
Veli Mustafayev <valibm@code.edu.az>
2022-06-07 17:45:47 +0400

    updated main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-//validate form
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
 const validate = {
     password: {
         length: "Password length must be atleast 8 characters",
@@ -26,11 +30,11 @@ const registerForm = document.querySelector(".regForm");
 //profile
 const profile = document.querySelector(".profileCard");
 
-//Local storage get function
+
 function localStorageGet(key) {
     return JSON.parse(localStorage.getItem(key));
 }
-//Local storage set function
+
 function localStorageSet(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
@@ -65,7 +69,7 @@ registerForm?.addEventListener('submit', (e) => {
 });
 
 
-//Check username
+
 function CheckEmail(email) {
     return userState.find((user) => user.email == email);
 }
@@ -86,7 +90,7 @@ loginForm?.addEventListener('submit', (e) => {
     }
 });
 
-//Validation Input
+
 function validateInput(input, regex, message) {
     const newRegex = new RegExp(regex);
     if (newRegex.test(input?.value)) {
@@ -120,75 +124,78 @@ emailInput?.addEventListener('keyup', function () {
     }
 });
 
-// (() => {
-//  if (sessionStorage.getItem("isLogin") != "true" || !localStorageGet(userLogin)) {
-//   profile?.innerHTML = `<div class="pCardHeader row justify-content-center">
-//      <div class="profileStats col-lg-4 col-md-4 d-flex">
-//       <div class="stat">
-//        <span class="statHeader">22</span>
-//        <span class="statBody">Friends</span>
-//       </div>
-//       <div class="stat">
-//        <span class="statHeader">10</span>
-//        <span class="statBody">Photos</span>
-//       </div>
-//       <div class="stat">
-//        <span class="statHeader">89</span>
-//        <span class="statBody">Comments</span>
-//       </div>
-//      </div>
-//      <div class="profilePicture col-lg-3 col-md-3">
-//       <img class="btnTransition" src="https://demos.creative-tim.com/argon-design-system-angular/assets/img/theme/team-4-800x800.jpg" alt="profile picture">
-//      </div>
-//      <div class="profileActions col-lg-4 col-md-4">
-//       <a class="btn btn-sm btn-info btnHover btnTransition fw-600 connectBtn" href="">CONNECT</a>
-//       <a class="btn btn-sm btnHover btnTransition fw-600 messageBtn" href="">MESSAGE</a>
-//      </div>
-//     </div>
-//     <div class="pCardBody text-center">
-//         <h3 class="fw-600">Jessica Jones <span class="fw-light">, 27</span></h3>
-//         <p class="fw-light location">Bucharest, Romania</p>
-//         <br>
-//         <p class="occupation">Solution Manager - Creative Tim Officer</p>
-//         <p class="education">University of Computer Science</p>
-//     </div>
-//     <div class="pCardFooter py-5 text-center">
-//         <div class="row justify-content-center">
-//             <div class="col-lg-9">
-//              <p class="cardFooterText">An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
-//              <p class="showMore">Show more</p>
-//             </div>
-//         </div>
-//     </div>`
-//  } else {
-//   profile?.innerHTML = `<div class="pCardHeader row justify-content-center">
-//      <div class="profileStats col-lg-4 d-flex">
-//       <div class="stat">
-//        <span class="statHeader">0</span>
-//        <span class="statBody">Friends</span>
-//       </div>
-//       <div class="stat">
-//        <span class="statHeader">0</span>
-//        <span class="statBody">Photos</span>
-//       </div>
-//       <div class="stat">
-//        <span class="statHeader">0</span>
-//        <span class="statBody">Comments</span>
-//       </div>
-//      </div>
-//      <div class="profilePicture col-lg-3">
-//       <img class="btnTransition" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" alt="profile picture">
-//      </div>
-//      <div class="profileActions col-lg-4">
-//       <a class="btn btn-sm btn-info btnHover btnTransition fw-600 connectBtn" href="">CONNECT</a>
-//       <a class="btn btn-sm btnHover btnTransition fw-600 messageBtn" href="">MESSAGE</a>
-//      </div>
-//     </div>
-//     <div class="pCardBody text-center">
-//         <h3 class="fw-600"> ${userLoginData.name}</h3>
-//     </div>`
-//  }
-// })();
+(() => {
+    console.log(profile);
+    if (profile != null) {
+        if (sessionStorage.getItem("isLogin") != "true" || !localStorageGet(userLogin)) {
+            profile.innerHTML = `<div class="pCardHeader row justify-content-center">
+               <div class="profileStats col-lg-4 col-md-4 d-flex">
+                <div class="stat">
+                 <span class="statHeader">22</span>
+                 <span class="statBody">Friends</span>
+                </div>
+                <div class="stat">
+                 <span class="statHeader">10</span>
+                 <span class="statBody">Photos</span>
+                </div>
+                <div class="stat">
+                 <span class="statHeader">89</span>
+                 <span class="statBody">Comments</span>
+                </div>
+               </div>
+               <div class="profilePicture col-lg-3 col-md-3">
+                <img class="btnTransition" src="https://demos.creative-tim.com/argon-design-system-angular/assets/img/theme/team-4-800x800.jpg" alt="profile picture">
+               </div>
+               <div class="profileActions col-lg-4 col-md-4">
+                <a class="btn btn-sm btn-info btnHover btnTransition fw-600 connectBtn" href="">CONNECT</a>
+                <a class="btn btn-sm btnHover btnTransition fw-600 messageBtn" href="">MESSAGE</a>
+               </div>
+              </div>
+              <div class="pCardBody text-center">
+                  <h3 class="fw-600">Jessica Jones <span class="fw-light">, 27</span></h3>
+                  <p class="fw-light location">Bucharest, Romania</p>
+                  <br>
+                  <p class="occupation">Solution Manager - Creative Tim Officer</p>
+                  <p class="education">University of Computer Science</p>
+              </div>
+              <div class="pCardFooter py-5 text-center">
+                  <div class="row justify-content-center">
+                      <div class="col-lg-9">
+                       <p class="cardFooterText">An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
+                       <p class="showMore">Show more</p>
+                      </div>
+                  </div>
+              </div>`
+           } else {
+            profile.innerHTML = `<div class="pCardHeader row justify-content-center">
+               <div class="profileStats col-lg-4 d-flex">
+                <div class="stat">
+                 <span class="statHeader">0</span>
+                 <span class="statBody">Friends</span>
+                </div>
+                <div class="stat">
+                 <span class="statHeader">0</span>
+                 <span class="statBody">Photos</span>
+                </div>
+                <div class="stat">
+                 <span class="statHeader">0</span>
+                 <span class="statBody">Comments</span>
+                </div>
+               </div>
+               <div class="profilePicture col-lg-3">
+                <img class="btnTransition" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" alt="profile picture">
+               </div>
+               <div class="profileActions col-lg-4">
+                <a class="btn btn-sm btn-info btnHover btnTransition fw-600 connectBtn" href="">CONNECT</a>
+                <a class="btn btn-sm btnHover btnTransition fw-600 messageBtn" href="">MESSAGE</a>
+               </div>
+              </div>
+              <div class="pCardBody text-center">
+                  <h3 class="fw-600"> ${userLoginData.name}</h3>
+              </div>`
+           }
+    }
+})();
 
 
 // tab-start
